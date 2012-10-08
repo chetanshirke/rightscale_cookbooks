5e0aa4b2eab387b3f760b5bd607022ececee4c0a
chetan shirke <chetanis1@gmail.com>
2012-10-08 22:56:30 +0530

    Create /home directory for chetan

---
+++ cookbooks/chetan/metadata.rb
@@ -0,0 +1,12 @@
+maintainer       "RightScale, Inc."
+maintainer_email "support@rightscale.com"
+license          "Copyright RightScale, Inc. All rights reserved."
+description      "Installs /home directory"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "13.2.0"
+
+# supports "centos", "~> 5.8", "~> 6"
+# supports "redhat", "~> 5.8"
+# supports "ubuntu", "~> 10.04", "~> 12.04"
+
+recipe "chetan::default", "This create directory in home folder."
---
+++ cookbooks/chetan/providers/default.rb
@@ -0,0 +1,11 @@
+
+
+
+
+  directory "/home/chetan" do
+    owner "root"
+    group "root"
+    mode 0700
+    recursive true
+    action :create
+  end
