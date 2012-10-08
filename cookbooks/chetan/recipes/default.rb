c2767129e14018920157991d78ed44bef5e4d5b1
chetan shirke <chetanis1@gmail.com>
2012-10-08 23:46:14 +0530

    recipe changed

--- cookbooks/chetan/recipes/default.rb
+++ cookbooks/chetan/recipes/default.rb
@@ -1,1 +1,11 @@
-provider "chetan"
+rightscale_marker :begin
+
+    directory "/home/chetan" do
+    owner "root"
+    group "root"
+    mode 0700
+    recursive true
+    action :create
+  end
+
+rightscale_marker :end
