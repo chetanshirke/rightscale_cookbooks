3f5fe9714167f199e3145230b5a978b59743b6da
chetan shirke <chetanis1@gmail.com>
2012-10-12 21:22:57 +0530

    resources added

--- cookbooks/chetan/metadata.rb
+++ cookbooks/chetan/metadata.rb
@@ -14,11 +14,11 @@ depends "rightscale"
 recipe  "chetan::default", "Installs the vsftpd application server."
 recipe  "chetan::do_stop", "Stops service."
 
-attribute "package",
+attribute "chetan",
   :display_name => "Import/export settings for database dump file management.",
   :type => "hash"
 
-attribute "package/ftp",
+attribute "chetan/package",
   :display_name => "package name",
   :description => "package name to install",
   :required => "required",
