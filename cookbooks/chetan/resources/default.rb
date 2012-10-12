56c8d7f9c51f60f85a3410e3a9c9c85a6e957f3d
chetan shirke <chetanis1@gmail.com>
2012-10-12 18:05:32 +0530

    resources added

---
+++ cookbooks/chetan/resources/default.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook Name:: chetan
+# Resource:: chetan::default
+#
+# Copyright RightScale, Inc. All rights reserved.  All access and use subject to the
+# RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
+# if applicable, other agreements such as a RightScale Master Subscription Agreement.
+
+
+# Install packages required for application server setup
+actions :install
+  # Set of packages to be installed in addition to the base application packages.
+  attribute :packages, :kind_of => Array
+
