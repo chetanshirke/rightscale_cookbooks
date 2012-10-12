e02bd8597b7984a63ee6a12ad48a3d1cde08afe6
chetan shirke <chetanis1@gmail.com>
2012-10-12 17:53:12 +0530

    resources added

--- cookbooks/chetan/providers/default.rb
+++ cookbooks/chetan/providers/default.rb
@@ -1,3 +1,13 @@
+#
+# Cookbook Name:: chetan
+#
+# Copyright RightScale, Inc. All rights reserved.  All access and use subject to the
+# RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
+# if applicable, other agreements such as a RightScale Master Subscription Agreement.
+
+# = Default application server provider
+#
+
 action :stop do
   log "  Running stop sequence"
   service "vsftpd" do
