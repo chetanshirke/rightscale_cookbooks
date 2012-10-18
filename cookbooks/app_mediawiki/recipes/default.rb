28b8c7ef4ecc69f79f6e426bcc6597ff7981a9e8
chetan shirke <chetanis1@gmail.com>
2012-10-18 20:18:26 +0530

    metadata.json updated

--- cookbooks/app_mediawiki/recipes/default.rb
+++ cookbooks/app_mediawiki/recipes/default.rb
@@ -11,7 +11,7 @@ rightscale_marker :begin
   user "root"
   cwd "/tmp"
   code <<-EOH
-  /usr/bin/wget http://download.wikimedia.org/mediawiki/1.19/mediawiki-1.19.2.tar.gz
+  /usr/bin/wget http://download.wikimedia.org/mediawiki/1.19/mediawiki-1.19.2.tar.gz -O /tmp/mediawiki-1.19.2.tar.gz
   /bin/tar -zxf /tmp/mediawiki-1.19.2.tar.gz
   /bin/mv mediawiki-1.19.2 /var/www/mediawiki
     EOH
