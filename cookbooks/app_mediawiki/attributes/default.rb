c0cbb01b83761ba5b2b1fdb0e92ce5fcdc904d07
chetan shirke <chetanis1@gmail.com>
2012-10-19 17:19:07 +0530

    app_media integrated with app_php and db-connection

---
+++ cookbooks/app_mediawiki/attributes/default.rb
@@ -0,0 +1,26 @@
+#
+# Cookbook Name:: app_mediawiki
+#
+# Copyright RightScale, Inc. All rights reserved.  All access and use subject to the
+# RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
+# if applicable, other agreements such as a RightScale Master Subscription Agreement.
+
+# List of additional php modules
+set_unless[:app_mediawiki][:modules_list] = []
+
+# Optional attributes
+# By default php uses MySQL as the DB adapter
+set_unless[:app][:db_adapter] = "mysql"
+set_unless[:app_mediawiki][:namespace] = "default"
+set_unless[:app_mediawiki][:admin_user] = "admin"
+set_unless[:app_mediawiki][:admin_pass] = "admin123"
+set_unless[:app_mediawiki][:email_account] = "root@localhost"
+# Calculated attributes
+# Defining apache user, module dependencies, and database adapter parameters depending on platform.
+case platform
+when "ubuntu"
+  set[:app_mediawiki][:module_dependencies] = [ "proxy_http", "php5" ]
+when "centos", "redhat"
+  set[:app_mediawiki][:module_dependencies] = [ "proxy", "proxy_http" ]
+end
+
---
+++ cookbooks/app_mediawiki/metadata.rb
@@ -0,0 +1,56 @@
+maintainer       "RightScale, Inc."
+maintainer_email "support@rightscale.com"
+license          "Copyright RightScale, Inc. All rights reserved."
+description      "Installs the php application server."
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "13.2.0"
+
+# supports "centos", "~> 5.8", "~> 6"
+# supports "redhat", "~> 5.8"
+# supports "ubuntu", "~> 10.04", "~> 12.04"
+
+depends "app"
+depends "repo"
+depends "rightscale"
+depends "web_apache"
+
+recipe  "app_mediawiki::default", "Installs the php and mediwiki application server."
+
+attribute "app_mediawiki",
+  :display_name => "PHP Mediawiki Application Settings",
+  :type => "hash"
+
+attribute "app_mediawiki/modules_list",
+  :display_name => "PHP module packages",
+  :description => "An optional list of php module packages to install. Accepts an array of package names. When using CentOS, package names are prefixed with php53u instead of php. To see a list of available php modules on CentOS, run 'yum search php53u' on the server. Example: php53u-mysql, php53u-pecl-memcache",
+  :required => "optional",
+  :type => "array",
+  :recipes => ["app_mediawiki::default"]
+
+attribute "app_mediawiki/namespace",
+  :display_name => "Mediawiki Namespace",
+  :description => "Mediawiki Namespace",
+  :required => "recommended",
+  :default => "default",
+  :recipes => ["app_mediawiki::default"]
+
+attribute "app_mediawiki/admin_user",
+  :display_name => "Mediawiki admin user account",
+  :description => "Mediawiki admin user account",
+  :required => "recommended",
+  :default => "admin",
+  :recipes => ["app_mediawiki::default"]
+
+attribute "app_mediawiki/admin_pass",
+  :display_name => "Mediawiki admin user account password",
+  :description => "Mediawiki admin user account password",
+  :required => "recommended",
+  :default => "admin123",
+  :recipes => ["app_mediawiki::default"]
+
+attribute "app_mediawiki/admin_email",
+  :display_name => "Mediawiki admin email account",
+  :description => "Mediawiki admin email account",
+  :required => "recommended",
+  :default => "root@localhost",
+  :recipes => ["app_mediawiki::default"]
