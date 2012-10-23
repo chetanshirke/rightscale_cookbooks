bea5ffb6ed4357c3c4d11518d375928281b3d5eb
chetan shirke <chetanis1@gmail.com>
2012-10-23 23:59:22 +0530

    application code update removed from provider and added new recipe do_install_mediawiki

--- cookbooks/app_mediawiki/recipes/do_update_code.rb
+++ cookbooks/app_mediawiki/recipes/do_update_code.rb
@@ -1,20 +1,27 @@
 #
-# Cookbook Name::app
+# Cookbook Name:: app_mediawiki
 #
-# Copyright RightScale, Inc. All rights reserved.  All access and use subject to the
+# Copyright RightScale, Inc. All rights reserved. All access and use subject to the
 # RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
 # if applicable, other agreements such as a RightScale Master Subscription Agreement.
 
-# This recipe will call "code_update" action of "app" LightWeight resource
-#  For more info please see "code_update" action definition in connected LightWeight provider (ex. app_php/providers/default.rb)
 
-rightscale_marker :begin
+  # Downloading app from URL
+package_name = "echo [:app_mediawiki][:download_url] | awk -F"/" '{print $(NF-0)}'"
+file_name = "echo package_name | cut -d. -f1,2,3"
 
-log "  Updating project code repository"
-
-app "default" do
-  destination node[:app_mediawiki][:destination]
-  action :code_update
+remote_file "/tmp/#{package_name}" do
+        source "[:app_mediawiki][:download_url]"
+        notifies :run, "bash[install_program]", :immediately
 end
 
-rightscale_marker :end
+bash "install_program" do
+  user "root"
+  cwd "/tmp"
+  code <<-EOH
+  tar -zxf #{package_name}
+  cp -a #{file_name} "#node[:app][:destination]" && rm -rf #{file_name}*
+   EOH
+  action :nothing
+end
+
