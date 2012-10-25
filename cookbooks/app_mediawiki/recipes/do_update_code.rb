e6897fe46ca8f3e6ebdcbc7a30f4c5523d46190c
chetan shirke <chetanis1@gmail.com>
2012-10-25 17:49:19 +0530

    application code update removed from provider and added new recipe do_install_mediawiki

--- cookbooks/app_mediawiki/recipes/do_update_code.rb
+++ cookbooks/app_mediawiki/recipes/do_update_code.rb
@@ -9,12 +9,12 @@
   # Downloading app from URL
 #package_name = "echo [:app_mediawiki][:download_url] | awk -F"/" '{print $(NF-0)}'"
 #file_name = "echo package_name | cut -d. -f1,2,3"
-ex = "#{node[:app_mediawiki][:download_url]}"
+ex = node[:app_mediawiki][:download_url]
 package_name = ex.split('/')
 file_name = package_name.split('.')
 
 remote_file "/tmp/#{package_name.last}" do
-        source "[:app_mediawiki][:download_url]"
+        source node[:app_mediawiki][:download_url]
         notifies :run, "bash[install_program]", :immediately
 end
 
@@ -22,8 +22,8 @@ bash "install_program" do
   user "root"
   cwd "/tmp"
   code <<-EOH
-  tar -zxf #{package_name.last}
-  cp -a "#{file_name.first}/*" "#node[:app][:destination]/" && rm -rf #{file_name.first}*
+  tar -zxf #{package_name.last}'
+  cp -a "#{file_name.first}/*" "node[:app][:destination]"+"/" && rm -rf #{file_name.first}*
    EOH
   action :nothing
 end
