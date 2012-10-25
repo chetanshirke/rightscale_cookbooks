6a57b25a633533ebfc2ef672351673490094de59
chetan shirke <chetanis1@gmail.com>
2012-10-25 18:05:19 +0530

    application code update removed from provider and added new recipe do_install_mediawiki

--- cookbooks/app_mediawiki/recipes/do_update_code.rb
+++ cookbooks/app_mediawiki/recipes/do_update_code.rb
@@ -7,13 +7,14 @@
 
 
   # Downloading app from URL
-#package_name = "echo [:app_mediawiki][:download_url] | awk -F"/" '{print $(NF-0)}'"
-#file_name = "echo package_name | cut -d. -f1,2,3"
-ex = node[:app_mediawiki][:download_url]
-package_name = ex.split('/')
-file_name = package_name.split('.')
 
-remote_file "/tmp/#{package_name.last}" do
+package_name = node[:app_mediawiki][:download_url]
+package_name = package_name.split('/')
+
+file_name = package_name.split('.')
+file_name = "#{file_name[0]}.#{file_name[1]}.#{file_name[2]}"
+
+remote_file "#{package_name.last}" do
         source node[:app_mediawiki][:download_url]
         notifies :run, "bash[install_program]", :immediately
 end
@@ -23,7 +24,7 @@ bash "install_program" do
   cwd "/tmp"
   code <<-EOH
   tar -zxf #{package_name.last}'
-  cp -a "#{file_name.first}/*" "node[:app][:destination]"+"/" && rm -rf #{file_name.first}*
+  cp -a "#{file_name}/*" "node[:app][:destination]"+"/" && rm -rf #{file_name}*
    EOH
   action :nothing
 end
