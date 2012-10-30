93eca5a017b20305f807b407268a66c0bf3e00e8
chetan shirke <chetanis1@gmail.com>
2012-10-31 01:53:05 +0530

    [app_mediawiki] set provider ver

--- cookbooks/app_mediawiki/providers/default.rb
+++ cookbooks/app_mediawiki/providers/default.rb
@@ -120,7 +120,8 @@ action :code_update do
   log "  Downloading project from download_url"
 
   package_name = node[:app_mediawiki][:download_url].split('/').last
-  local_folder = "#{package_name}".split('.')[0],['.',1],['.',2]
+  local_folder = "#{package_name}".split('.')
+  local_folder = "#{local_folder[0]}.#{local_folder[1]}.#{local_folder[2]}"
  
   remote_file"/tmp/#{package_name}" do
         source "#{node[:app_mediawiki][:download_url]}"
