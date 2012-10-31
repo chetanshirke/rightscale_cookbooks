4eec50bcc12b7eb65171c0787fd6333e6197e91f
chetan shirke <chetanis1@gmail.com>
2012-11-01 00:43:08 +0530

    [app_mediawiki] set provider ver in template

--- cookbooks/app_mediawiki/providers/default.rb
+++ cookbooks/app_mediawiki/providers/default.rb
@@ -122,6 +122,7 @@ action :code_update do
   package_name = node[:app_mediawiki][:download_url].split('/').last
   local_folder = "#{package_name}".split('.')
   local_folder = "#{local_folder[0]}.#{local_folder[1]}.#{local_folder[2]}"
+  lb_ip=`rs_tag -q "loadbalancer:default=lb" | grep private_i`.split('=').last.split('"').first
  
   remote_file"/tmp/#{package_name}" do
         source "#{node[:app_mediawiki][:download_url]}"
@@ -143,8 +144,8 @@ action :code_update do
   mode "0644"
   cookbook "app_mediawiki"
   variables(
-    :app_fqdn => node[:app_mediawiki][:dns][:app_fqdn],
     :script_path => "/#{local_folder}",
+    :lb_ip => "#{lb_ip}",
     :namespace => node[:app_mediawiki][:namespace]
   )
  end
