1ce137d931aea687c7e2016d00cffa9d0d6792e9
chetan shirke <chetanis1@gmail.com>
2012-11-01 20:54:28 +0530

    [app_mediawiki] db server added to template

--- cookbooks/app_mediawiki/providers/default.rb
+++ cookbooks/app_mediawiki/providers/default.rb
@@ -123,7 +123,7 @@ action :code_update do
   local_folder = "#{package_name}".split('.')
   local_folder = "#{local_folder[0]}.#{local_folder[1]}.#{local_folder[2]}"
   lb_ip=`rs_tag -q "loadbalancer:default=lb" | grep private_i`.split('=').last.split('"').first
- 
+  db_ip=`rs_tag -q "database:active=true" |grep private_i`.split('=').last.split('"').first
   remote_file"/tmp/#{package_name}" do
         source "#{node[:app_mediawiki][:download_url]}"
         notifies :run, "bash[install_program]", :immediately
@@ -146,6 +146,7 @@ action :code_update do
   variables(
     :script_path => "/#{local_folder}",
     :lb_ip => "#{lb_ip}",
+    :db_ip => "#{db_ip}",
     :namespace => node[:app_mediawiki][:namespace]
   )
  end
